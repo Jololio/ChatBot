25b5d4261c3c717a295cf57923736e08398b8081
Joseph Al-Abudi <jala9132@740s-pl-120.local>
2017-03-10 13:52:02 -0700

    Added methods to code (errors exist, although code is incomplete)

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,5 +1,6 @@
 package chat.model;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -53,9 +54,111 @@ public class CTECTwitter
 	
 	public String getMostCommonWord()
 	{
-		return null;
+		String results = "";
+		collectTweets(user);
+		turnStatusesToWords();
+		
+		removeAllBoringWords();
+		removeEmptyText();
+		results += "from " + user + calculatePopularWordAndCount();
+		return results;
 	}
 	
+	private void turnStatusesToWords()
+	{
+		for(Status currentStatus : searchedTweets)
+		{
+			String tweetText = currentStatus.getText();
+			String [] tweetWords = tweetText.split(" ");
+			for(int index = 0; index < tweetWords.length; index++)
+			{
+				tweetedWords.add(tweetWords[index]);
+			}
+		}
+	}
+	
+	private void removeAllBoringWords()
+	{
+		String [] boringWords = createIgnoredWordArray();
+		for(int index = 0; index < tweetedWords.size(); index++)
+		{
+			for(int boringIndex = 0; boringIndex < boringWords.length; boringIndex++)
+			{
+				if(tweetedWords.get(index).equalsIgnoreCase(boringWords[boringIndex]))
+				{
+					tweetedWords.remove(index);
+					index--;
+					boringIndex = boringWords.length;
+				}
+			}
+		}
+	}
+	
+	private void removeEmptyText()
+	{
+		for(int index = tweetedWords.size() - 1; index >= 0; index--)
+		{
+			if(tweetedWords.get(index).trim().equals(""))
+			{
+				tweetedWords.remove(index);
+			}
+		}
+	}
+	
+	private String calculatePopularWordAndCount()
+	{
+		String information = "";
+		String mostPopular = "";
+		int popularIndex = -1;
+		int popularCount = 0;
+		
+		for(int index = 0; index < tweetedWords.size(); index++)
+		{
+			int currentPopularity = 0;
+			for(int searched = index + 1; searched < tweetedWords.size(); searched++)
+			{
+				if(tweetedWords.get(index).equalsIgnoreCase(tweetedWords.get(searched)) && !tweeted.get(index).equals(mostPopular))
+				{
+					currentPopularity++;
+				}
+			}
+			if(currentPopularity > popularCount)
+			{
+				popularIndex = index;
+				popularCount = currentPopularity;
+				mostPopular = tweetedWords.get(index);
+			}
+		}
+		information = " the most popular word is: " + mostPopular + ", and it occured " + popularCount + " times out of " + tweetedWords.size() + ", AKA " + (DecimalFormat.getPercentInstance().format(((double) popularCount)/tweetedWords.size()));
+		return information;
+	}
+	
+	private String removePunctuation(String currentString)
+	{
+		String punctuation = ".,'?!:;\"(){}[]^<>-";
+		
+		String scrubbedString = "";
+		for(int i = 0; i < currentString.length(); i++)
+		{
+			if(punctuation.indexOf(currentString.charAt(i)) == -1)
+			{
+				scrubbedString += currentString.charAt(i);
+			}
+		}
+		return scrubbedString;
+	}
 
+	public String getMostPopularWord(String username)
+	{
+		gatherTheTweets(username);
+		turnTweetsToWords();
+		removeBoringWords();
+		removeBlankWords();
+		
+		String information = "The tweet count is " + allTheTweets.size() + " and " + username + "'s " + calculateTopWord();
+		return information;
+	}
+
+	
 }
 
